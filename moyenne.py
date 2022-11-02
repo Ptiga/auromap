21157967c91bd4eb926b0ea8687f281ab286a936
Ptiga <tylerdurdenxp@gmail.com>
2022-11-02 12:25:04 +0100

    test unitaires auto

---
+++ moyenne.py
@@ -0,0 +1,4 @@
+def moyenne(a, b):
+    return(a+b)/2
+
+#print(moyenne(2,4))
---
+++ test_moyenne.py
@@ -0,0 +1,15 @@
+import moyenne as moy
+
+def testMoyenne():
+    a = 2
+    b = 4
+
+    result = moy.moyenne(a,b)
+
+    if result!=3:
+        print('Test échoué')
+    else:
+        print('Test réussi')
+
+testMoyenne()
+
